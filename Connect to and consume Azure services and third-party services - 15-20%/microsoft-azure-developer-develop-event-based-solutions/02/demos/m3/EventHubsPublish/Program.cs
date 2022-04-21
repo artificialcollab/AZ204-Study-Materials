07b571cb69dd3a9ce0d33d48988c1e5e772094bc
ChaosTechOps <43592368+ctrlshiftesc-org@users.noreply.github.com>
2022-04-20 20:38:48 -0400

    initial upload of study materials
    initial upload of study materials

---
+++ Connect to and consume Azure services and third-party services - 15-20%/microsoft-azure-developer-develop-event-based-solutions/02/demos/m3/EventHubsPublish/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Collections.Generic;
+
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+
+namespace EventHubsPublish
+{
+    class Program
+    {  
+        static Random rand = new Random();
+
+        private const string connectionString = "";
+            
+        private const string eventHubName = "";
+             
+        static async Task Main(string[] args)
+        {  
+            await SendToSamePartition();                   
+        }
+
+        static async Task SendToRandomPartition()
+        {            
+            await using var producerClient = 
+                new EventHubProducerClient(connectionString, eventHubName);
+
+            using EventDataBatch eventBatch = 
+                await producerClient.CreateBatchAsync();
+                            
+            for (int i = 0; i < 100; i++)
+            {
+                double waterTemp = (rand.NextDouble()) * 100;
+                int coffeeTypeIndex = rand.Next(2);
+
+                var coffeeMachineData = new CoffeeData
+                { 
+                    WaterTemperature = waterTemp, 
+                    ReadingTime = DateTime.Now, 
+                    CoffeeType = CoffeeData.AllCoffeeTypes[coffeeTypeIndex]
+                };
+
+                var coffeeMachineDataBytes = 
+                    JsonSerializer.SerializeToUtf8Bytes(coffeeMachineData);
+
+                var eventData = new EventData(coffeeMachineDataBytes);
+
+                if (!eventBatch.TryAdd(eventData))
+                {
+                    throw new Exception("Cannot add coffee machine data to random batch");
+                }
+            }
+
+            await producerClient.SendAsync(eventBatch); 
+
+            Console.WriteLine("Wrote events to random partitions");           
+        }
+        
+        static async Task SendToSamePartition()
+        {            
+            await using var producerClient = 
+                new EventHubProducerClient(connectionString, eventHubName);
+            
+            // can also do this with EventDataBatch - but showing another way
+            
+            List<EventData> eventBatch = new List<EventData>();
+                
+            for (int i = 0; i < 100; i++)
+            {
+                double waterTemp = (rand.NextDouble()) * 100;
+                int coffeeTypeIndex = rand.Next(2);
+
+                var coffeeMachineData = new CoffeeData
+                { 
+                    WaterTemperature = waterTemp, 
+                    ReadingTime = DateTime.Now, 
+                    CoffeeType = CoffeeData.AllCoffeeTypes[coffeeTypeIndex]
+                };
+            
+                var coffeeMachineDataBytes = 
+                    JsonSerializer.SerializeToUtf8Bytes(coffeeMachineData);
+
+                var eventData = new EventData(coffeeMachineDataBytes);
+                             
+                eventBatch.Add(eventData);                
+            }
+
+            var options = new SendEventOptions();
+            options.PartitionKey = "device1";
+            
+            await producerClient.SendAsync(eventBatch, options);
+
+            Console.WriteLine("Wrote events to single partition");
+        }
+
+        
+    }    
+}
---
+++ Connect to and consume Azure services and third-party services - 15-20%/microsoft-azure-developer-develop-event-based-solutions/03/demos/SendEvent/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.EventGrid.Models;
+using Microsoft.Azure.EventGrid;
+
+namespace SendEvent
+{
+    class Program
+    {
+        
+        async static Task Main(string[] args)
+        {
+            string endpoint = "";
+            string key = "";
+            string topicHostName = new Uri(endpoint).Host;
+
+            var newAccount = new NewAccountMessage { AccountName = "Matt", SourceSystem = "Pluralsight Course" };
+
+            EventGridEvent newAccountEvent = new EventGridEvent(
+                id: Guid.NewGuid().ToString(), 
+                subject: "New Account", 
+                data: newAccount, 
+                eventType: "NewAccountCreated", 
+                eventTime: DateTime.Now, 
+                dataVersion: "1.0");
+            
+            TopicCredentials credentials = new TopicCredentials(key);
+
+            EventGridClient client = new EventGridClient(credentials);
+
+            await client.PublishEventsAsync(topicHostName, new EventGridEvent[] { newAccountEvent });
+        }
+    }
+}
